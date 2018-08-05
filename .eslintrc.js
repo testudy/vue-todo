849718ba39077c173a121ea40987804c15367dd5
胡继伟 <hu_jiwei@foxmail.com>
2018-08-06 00:04:43 +0800

    忽略对参数属性修改的警告，主要针对vuex中的mutations

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'indent': ['error', 4],
     'max-len': ['error', { 'code': 120 }],
+    'no-param-reassign': ["error", { "props": false }],
     'vue/attribute-hyphenation': [
       'error',
       'always'
