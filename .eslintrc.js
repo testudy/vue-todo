eb8fe95508f6ce3b13e8702aa767821e875b74c2
胡继伟 <hu_jiwei@foxmail.com>
2018-08-07 08:58:39 +0800

    调整.eslintrc.js格式，适应Vue UI的格式

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': ['error', 4],
-    'max-len': ['error', { 'code': 120 }],
-    'no-param-reassign': ["error", { "props": false }],
+    indent: ['error', 4],
+    'max-len': ['error', { code: 120 }],
+    'no-param-reassign': ['error', { props: false }],
     'vue/attribute-hyphenation': [
       'error',
       'always'
